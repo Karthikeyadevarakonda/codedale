0c4c26c0048186a7e32ffca82e7c7991c13e9518
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2026-01-31 14:40:03 +0530

    added

--- src/ScrollSequenceHero.jsx
+++ src/ScrollSequenceHero.jsx
@@ -35,7 +35,7 @@ export default function ScrollSequenceHero({ imageUrls }) {
   return (
     <section
       ref={sectionRef}
-      className="relative w-full bg-gray-100 overscroll-none"
+      className="relative w-full bg-amber-200 overscroll-none"
       style={{ height: `${images.length * 3}vh` }}
     >
       <div className="sticky top-0 h-[100svh] w-screen overflow-hidden">
